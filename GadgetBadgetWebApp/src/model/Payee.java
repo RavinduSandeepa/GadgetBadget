9cb302ce619b8e5d1433da42330606b6685558bf
RavinduSandeepa <ravindusundeepa100@gmail.com>
2021-04-23 23:55:04 +0530

    final commit buyer finalized

--- GadgetBadgetWebApp/src/model/Payee.java
+++ GadgetBadgetWebApp/src/model/Payee.java
@@ -16,7 +16,7 @@ public class Payee {
 							Class.forName("com.mysql.cj.jdbc.Driver");
 							
 							//Provide the correct details: DBServer/DBName, username, password 
-							con = DriverManager.getConnection("jdbc:mysql://localhost:3306/accountsdb","root","ravinduc3303");
+							con = DriverManager.getConnection("jdbc:mysql://localhost:3306/accountsdb","root","");
 							
 							//con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
 						} 
